529da9750de6a9030cf46c28b6dba21c0e75e069
millerding <mding022@uottawa.ca>
2025-01-18 22:28:20 -0500

    Change metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Project",
+  title: "SolaceFlow",
   description: "uOttaHack 2025",
 };
 
